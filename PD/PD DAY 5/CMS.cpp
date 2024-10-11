5bb018744f8cf247c3e16f6c5daa4bf2bfdb824b
numair42101 <numair42101@gmail.com>
2024-10-11 22:42:14 +0500

    PROJECT

--- PD/PD DAY 5/CMS.cpp
+++ PD/PD DAY 5/CMS.cpp
@@ -3,18 +3,34 @@
 using namespace std;
 
 void printheader();
-void printmainmenu();
+void printaccounttypes();
 int selectaccounttype();
-void accounttype(int selectaccount);
+void printaccountactions();
 int selectaccountaction();
-void accountaction(int selectaccountaction);
+void printaccountaction(int selectaccountaction, int accounttype);
+void studentsignup();
+void studentsignin();
+void teachersignup();
+void teachersignin();
+void adminsignup();
+void adminsignin();
 
 int main()
 {
+    int accounttype, accountaction;
     system("cls");
     printheader();
-    printmainmenu();
-    accounttype(selectaccounttype());
+    printaccounttypes();
+    accounttype = selectaccounttype();
+    if (accounttype == 0)
+    {
+        system("cls");
+        exit(0);
+    }
+    else
+        printaccountactions();
+    accountaction = selectaccountaction();
+    printaccountaction(accountaction, accounttype);
 }
 
 void printheader()
@@ -28,89 +44,159 @@ void printheader()
     cout << "******************************************************************************************************************************************************************        " << endl;
     cout << "" << endl;
 }
-void printmainmenu()
+void printaccounttypes()
 {
-    int selectaccount;
     cout << "1. STUDENT ACCOUNT" << endl;
     cout << "2. TEACHER ACCOUNT" << endl;
     cout << "3. ADMINISTRATOR ACCOUNT" << endl;
 }
 int selectaccounttype()
 {
-    int selectaccount;
+    int accounttype;
     cout << "Select your account or exit(0): ";
-    cin >> selectaccount;
-    return selectaccount;
+    cin >> accounttype;
+    return accounttype;
 }
-void accounttype(int selectaccount)
+void printaccountactions()
 {
-    if (selectaccount == 0)
-    {
-        system("cls");
-        exit(0);
-    }
-    else if (selectaccount == 1)
-    {
-        system("cls");
-        printheader();
-        cout << "1. SIGN UP" << endl;
-        cout << "2. SIGN IN" << endl;
-        cout << "3. BACK" << endl;
-        accountaction(selectaccountaction());
-    }
-    else if (selectaccount == 2)
-    {
-        system("cls");
-        printheader();
-        cout << "1. SIGN UP" << endl;
-        cout << "2. SIGN IN" << endl;
-        cout << "3. BACK" << endl;
-        accountaction(selectaccountaction());
-    }
-    else if (selectaccount == 3)
-    {
-        system("cls");
-        printheader();
-        cout << "1. SIGN UP" << endl;
-        cout << "2. SIGN IN" << endl;
-        cout << "3. BACK" << endl;
-        accountaction(selectaccountaction());
-    }
-    else
-    {
-        cout << "invalid";
-    }
+    system("cls");
+    printheader();
+    cout << "1. SIGN UP" << endl;
+    cout << "2. SIGN IN" << endl;
+    cout << "3. BACK" << endl;
 }
 int selectaccountaction()
 {
-    int selectaccountaction;
+    int accountaction;
     cout << "What do you want..." << endl;
-    cin >> selectaccountaction;
-    return selectaccountaction;
+    cin >> accountaction;
+    return accountaction;
 }
-void accountaction(int selectaccountaction)
+void studentsignup()
 {
-    if (selectaccountaction == 1)
+    string name, fathername, registrationnumber, password, cnic;
+    char section;
+    int age, rollno, session;
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Enter your father name : ";
+    cin >> fathername;
+    cout << "Enter your CNIC : ";
+    cin >> cnic;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "Enter your session : ";
+    cin >> session;
+    cout << "Enter your section : ";
+    cin >> section;
+    cout << "Enter your rollno : ";
+    cin >> rollno;
+    cout << "Enter your registrationnumber : ";
+    cin >> registrationnumber;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void studentsignin()
+{
+    string registrationnumber, password;
+    cout << "Enter your registrationnumber : ";
+    cin >> registrationnumber;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void teachersignup()
+{
+    string name, fathername, subject, cnic, password, email;
+    int age, session;
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Enter your father name : ";
+    cin >> fathername;
+    cout << "Enter your CNIC : ";
+    cin >> cnic;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "Enter your session (employed) : ";
+    cin >> session;
+    cout << "Enter your subject : ";
+    cin >> subject;
+    cout << "Enter your email : ";
+    cin >> email;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void teachersignin()
+{
+    string email, password;
+    cout << "Enter your email : ";
+    cin >> email;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void adminsignup()
+{
+    string name, cnic, fathername, password, email;
+    int age;
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Enter your father name : ";
+    cin >> fathername;
+    cout << "Enter your CNIC : ";
+    cin >> cnic;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "Enter your email : ";
+    cin >> email;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void adminsignin()
+{
+    string email, password;
+    cout << "Enter your email : ";
+    cin >> email;
+    cout << "Enter your password : ";
+    cin >> password;
+}
+void printaccountaction(int accountaction, int accounttype)
+{
+    if (accountaction == 1)
     {
         system("cls");
         printheader();
-        cout << "~~~CREATING YOUR ACCOUNT~~~" << endl;
+        cout << "~~~CREATING YOUR ACCOUNT~~~" << endl
+             << endl;
+        if (accounttype = 1)
+            studentsignup();
+        else if (accounttype = 2)
+            teachersignup();
+        else if (accounttype = 3)
+            adminsignup();
     }
-    else if (selectaccountaction == 2)
+    else if (accountaction == 2)
     {
         system("cls");
         printheader();
-        cout << "~~~ACCESSING YOUR ACCOUNT~~~" << endl;
+        cout << "~~~ACCESSING YOUR ACCOUNT~~~" << endl
+             << endl;
+        if (accounttype = 1)
+            studentsignin();
+        else if (accounttype = 2)
+            teachersignin();
+        else if (accounttype = 3)
+            adminsignin();
     }
-    else if (selectaccountaction == 3)
+    else if (accountaction == 3)
     {
         system("cls");
         printheader();
-        printmainmenu();
-        accounttype(selectaccounttype());
-    }
-    else
-    {
-        cout << "invalid";
+        printaccounttypes();
+        selectaccounttype();
+        printaccountactions();
+        selectaccountaction();
+        printaccountaction(accountaction,accounttype);
     }
 }
+
+
+
